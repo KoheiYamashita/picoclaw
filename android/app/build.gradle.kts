f9a374def403f9466f5ba0f76064094653aea81d
KoheiYamashita <15520963+KoheiYamashita@users.noreply.github.com>
2026-03-02 13:44:14 +0000

    chore: bump version to 0.4.0

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "io.clawdroid"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 3
+        versionCode = 4
         versionName = appVersionName
     }
 
